8b33143261fcbf72bf02e7c340bd5bed759bf01e
Emil Gildebrand <emil@gildebrand>
2026-08-04 22:03:25 +0200

    feat: add IncrementRouteAttempts helper

--- core/db-utils.go
+++ core/db-utils.go
@@ -26,3 +26,22 @@ func MarkProcessed(pg *postgres, colName string, aircrafts []Aircraft) {
 		}
 	}
 }
+
+func IncrementRouteAttempts(pg *postgres, aircrafts []Aircraft) {
+
+	batch := &pgx.Batch{}
+
+	for _, aircraft := range aircrafts {
+		batch.Queue(`UPDATE aircraft_data SET route_attempts = route_attempts + 1 WHERE id = $1`, aircraft.Id)
+	}
+
+	br := pg.db.SendBatch(context.Background(), batch)
+	defer br.Close()
+
+	for i := 0; i < len(aircrafts); i++ {
+		_, err := br.Exec()
+		if err != nil {
+			log.Error().Err(err).Msg("IncrementRouteAttempts() - Unable to update data")
+		}
+	}
+}
